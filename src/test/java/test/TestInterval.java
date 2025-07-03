9c7a9d87af611b51851292b2c68d70ce5d371e4a
Sylvain <sylvain.colomb@live.fr>
2025-07-03 22:09:35 +0200

    interval full test iter

--- src/test/java/test/TestInterval.java
+++ src/test/java/test/TestInterval.java
@@ -281,8 +281,20 @@ public class TestInterval extends TestCommon {
 
 		section("Interval.iter()");
 		code("var t = []; intervalIter([1..4], function(v) { push(t,v); }); return t;").equals("[1, 2, 3, 4]");
+		code("var t = []; intervalIter(]1..4], function(v) { push(t,v); }); return t;").equals("[2, 3, 4]");
+		code("var t = []; intervalIter([1..4[, function(v) { push(t,v); }); return t;").equals("[1, 2, 3]");
+		code("var t = []; intervalIter(]1..4[, function(v) { push(t,v); }); return t;").equals("[2, 3]");
 		code_v1("var t = []; intervalIter([1..4], function(@v) { push(t,v); }); return t;").equals("[1, 2, 3, 4]");
+		code_v1("var t = []; intervalIter(]1..4], function(@v) { push(t,v); }); return t;").equals("[2, 3, 4]");
+		code_v1("var t = []; intervalIter([1..4[, function(@v) { push(t,v); }); return t;").equals("[1, 2, 3]");
+		code_v1("var t = []; intervalIter(]1..4[, function(@v) { push(t,v); }); return t;").equals("[2, 3]");
 		code_v1_3("var t = []; intervalIter([1..4], function(k, v) { push(t,v); }); return t;").equals("[1, 2, 3, 4]");
+		code_v1_3("var t = []; intervalIter(]1..4], function(k, v) { push(t,v); }); return t;").equals("[2, 3, 4]");
+		code_v1_3("var t = []; intervalIter([1..4[, function(k, v) { push(t,v); }); return t;").equals("[1, 2, 3]");
+		code_v1_3("var t = []; intervalIter(]1..4[, function(k, v) { push(t,v); }); return t;").equals("[2, 3]");
 		code_v4_("var t = []; intervalIter([1..4], function(k, v) { push(t,v); }); return t;").equals("[1, 2, 3, 4]");
+		code_v4_("var t = []; intervalIter(]1..4], function(k, v) { push(t,v); }); return t;").equals("[2, 3, 4]");
+		code_v4_("var t = []; intervalIter([1..4[, function(k, v) { push(t,v); }); return t;").equals("[1, 2, 3]");
+		code_v4_("var t = []; intervalIter(]1..4[, function(k, v) { push(t,v); }); return t;").equals("[2, 3]");
 	}
 }
